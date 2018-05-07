2a806d19ab7dc4f19f185ba7ffacf0bcc78301c6
Chris DEV <softreaver@laposte.net>
2018-05-07 16:44:13 +0200

    MAJ 07/05/2018

--- MonApp/app/src/main/java/com/example/christophermilazzo/monapp/MainActivity.java
+++ MonApp/app/src/main/java/com/example/christophermilazzo/monapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.christophermilazzo.monapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.w("TAG", "onCreate: test warnning");
+
+        Button bt = null;
+        bt.setText("bouton");
     }
 }
